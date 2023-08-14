1ab454fa7355b2483c268c9cd6dac2eea0da267f
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-08-14 04:19:03 -0400

    The DB class for connection to the database was created, and the class DbException was also created for handling exceptions.

--- src/db/DBException.java
+++ src/db/DBException.java
@@ -1,5 +1,11 @@
 package db;
 
-public class DBException {
+public class DbException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
 
+	public DbException(String msg) {
+		super(msg);
+	}
+	
 }
